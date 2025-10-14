b04973bbc3aa39dc00eac08855d8fb0d33c6a0f9
joaoguilherme2008-rgb <joaoguilhermereis2008@gmail.com>
2025-10-14 10:36:03 -0300

    AAAAAAAA

--- Nextlanche/App.js
+++ Nextlanche/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Arthur é gay!</Text>
+      <Text>Guilherme é gay!</Text>
       <StatusBar style="auto" />
     </View>
   );
