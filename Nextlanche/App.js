5420bcdc04cfe2fb67796afcde7d41fa1801dc68
Arthur Leandro <arthurfonseca263@gmail.com>
2025-10-14 11:07:03 -0300

    app

--- Nextlanche/App.js
+++ Nextlanche/App.js
@@ -2,8 +2,6 @@ import React, { useEffect, useState } from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { supabase } from './services/supabase'
-
-// Telas
 import LoginScreen from './screens/loginScreens'
 import HomeScreen from './screens/homeScreens'
 import HistoricoScreen from './screens/historicoScreens'
