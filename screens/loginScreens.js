c1313c25d6beeb9e6ab8141df96f99b6d542757a
Arthur Leandro <arthurfonseca263@gmail.com>
2025-10-22 09:14:39 -0300

    mudança de pastas

--- screens/loginScreens.js
+++ screens/loginScreens.js
@@ -1,4 +1,3 @@
 import React, { useState } from 'react'
 import { View, TextInput, Button, Text, Alert } from 'react-native'
 import { supabase } from '../services/supabase'
-
--- services/supabase.js
+++ services/supabase.js
@@ -5,6 +5,3 @@ const SUPABASE_URL = 'https://xiloeolriymcqwzaikus.supabase.co'
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhpbG9lb2xyaXltY3F3emFpa3VzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA0MzAyNDQsImV4cCI6MjA3NjAwNjI0NH0.ZvLm35Kxb87D1lPHc_IZ6q-gpfYB_scUnj4WiZ5rUio'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
-
-
-
