c76cd99e9acabdada81c3e84d4fe09ceb30dbcfd
Arthur Leandro <arthurfonseca263@gmail.com>
2025-11-24 11:27:27 -0300

    bah

--- Nextlanche/App.js
+++ Nextlanche/App.js
@@ -3,12 +3,12 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { createStackNavigator } from "@react-navigation/stack";
 
-
 import SobreScreens from "./screens/sobreScreens";  
 import PerfilScreens from "./screens/perfilScreens"; 
 import HistoricoScreens from "./screens/historicoScreens";
 import CantinaScreens from "./screens/cantinaScreens";
 import DetalhesCompra from "./screens/detalhesCompra";
+import LoginScreens from "./screens/loginScreens";   // <-- ADICIONADO
 
 import FontAwesome from "react-native-vector-icons/FontAwesome";
 import HomeScreens from "./screens/homeScreens";
@@ -40,67 +40,87 @@ function CantinaStack() {
   );
 }
 
+function DrawerMenu() {
+  return (
+    <Drawer.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: "#2196f3" },
+        headerTintColor: "#fff",
+        drawerActiveTintColor: "#2196f3",
+      }}
+    >
+      <Drawer.Screen
+        name="Início"
+        component={HomeScreens}
+        options={{
+          drawerIcon: ({ color, size }) => (
+            <FontAwesome name="home" color={color} size={size} />
+          ),
+        }}
+      />
+
+      <Drawer.Screen
+        name="Cantina"
+        component={CantinaStack}
+        options={{
+          drawerIcon: ({ color, size }) => (
+            <FontAwesome name="cutlery" color={color} size={size} />
+          ),
+        }}
+      />
+
+      <Drawer.Screen
+        name="Histórico"
+        component={HistoricoScreens}
+        options={{
+          drawerIcon: ({ color, size }) => (
+            <FontAwesome name="list" color={color} size={size} />
+          ),
+        }}
+      />
+
+      <Drawer.Screen
+        name="Perfil"
+        component={PerfilScreens}
+        options={{
+          drawerIcon: ({ color, size }) => (
+            <FontAwesome name="user" color={color} size={size} />
+          ),
+        }}
+      />
+
+      <Drawer.Screen
+        name="Sobre"
+        component={SobreScreens}
+        options={{
+          drawerIcon: ({ color, size }) => (
+            <FontAwesome name="info-circle" color={color} size={size} />
+          ),
+        }}
+      />
+    </Drawer.Navigator>
+  );
+}
+
 export default function App() {
   return (
     <CartProvider>
       <NavigationContainer>
-        <Drawer.Navigator
-          screenOptions={{
-            headerStyle: { backgroundColor: "#2196f3" },
-            headerTintColor: "#fff",
-            drawerActiveTintColor: "#2196f3",
-          }}
-        >
-          <Drawer.Screen
-            name="Início"
-            component={HomeScreens}
-            options={{
-              drawerIcon: ({ color, size }) => (
-                <FontAwesome name="home" color={color} size={size} />
-              ),
-            }}
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          
+          {/* LOGIN PRIMEIRO */}
+          <Stack.Screen 
+            name="Login" 
+            component={LoginScreens} 
           />
 
-          <Drawer.Screen
-            name="Cantina"
-            component={CantinaStack}
-            options={{
-              drawerIcon: ({ color, size }) => (
-                <FontAwesome name="cutlery" color={color} size={size} />
-              ),
-            }}
+          {/* MENU APÓS LOGIN */}
+          <Stack.Screen 
+            name="MainApp" 
+            component={DrawerMenu}
           />
-
-          <Drawer.Screen
-            name="Histórico"
-            component={HistoricoScreens}
-            options={{
-              drawerIcon: ({ color, size }) => (
-                <FontAwesome name="list" color={color} size={size} />
-              ),
-            }}
-          />
-
-          <Drawer.Screen
-            name="Perfil"
-            component={PerfilScreens}
-            options={{
-              drawerIcon: ({ color, size }) => (
-                <FontAwesome name="user" color={color} size={size} />
-              ),
-            }}
-          />
-
-          <Drawer.Screen
-            name="Sobre"
-            component={SobreScreens}
-            options={{
-              drawerIcon: ({ color, size }) => (
-                <FontAwesome name="info-circle" color={color} size={size} />
-              ),
-            }}
-          />
-        </Drawer.Navigator>
+          
+        </Stack.Navigator>
       </NavigationContainer>
     </CartProvider>
   );
